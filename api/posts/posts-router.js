41a27d399051d8b6c280f3bb2b8cffc93f0cda31
beatlesm <rj.geng@gmail.com>
2022-02-08 13:02:18 -0800

    Task2: - [GET] /api/posts/:id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -13,4 +13,20 @@ router.get('/', (req, res, next) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+	Post.findById(req.params.id)
+		.then(post => {
+			if (post) {
+				res.status(200).json(post);
+			} else {
+				res.status(404).json({ message: "The post with the specified ID does not exist" });
+			}
+		})
+		.catch( () => {			
+			res.json({message: "The post information could not be retrieved",});
+		});
+});
+
+
+
 module.exports = router
