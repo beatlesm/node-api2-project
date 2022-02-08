d59496325c84b9237e558cba4fcc645e846b08c3
beatlesm <rj.geng@gmail.com>
2022-02-08 12:49:41 -0800

    Task2: - [GET] /api/posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,6 +1,16 @@
 // implement your posts router here
 const router = require("express").Router()
 
+const Post = require('./posts-model')
 
+router.get('/', (req, res, next) => {    
+    Post.find()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(() => {
+            res.json({ message: "The posts information could not be retrieved" })
+        })
+})
 
 module.exports = router
