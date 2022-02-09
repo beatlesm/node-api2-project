eb88dd60d1859ac8d58143e8a35df0179d883617
beatlesm <rj.geng@gmail.com>
2022-02-08 17:46:46 -0800

    Task2: - [GET] /api/posts/:id/comments

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -96,4 +96,22 @@ router.delete('/:id', (req, res) => {
 
 }); 
 
+router.get('/:id/comments', (req, res) => {
+	Post.findById(req.params.id)
+		.then(post => {
+			if (post) {
+				return Post.findPostComments(req.params.id)
+			} else {
+				res.status(404).json({ message: "The post with the specified ID does not exist" });
+			}
+		})
+        .then (comments =>  {
+            res.status(200).json(comments);
+        })
+		.catch( () => {			
+			res.json({message: "The comments information could not be retrieved"});
+		});
+	
+})
+
 module.exports = router
