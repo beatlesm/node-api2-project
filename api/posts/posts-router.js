cc046e85eab3f539783c46d5b5dcbf8b66964d66
beatlesm <rj.geng@gmail.com>
2022-02-08 16:18:38 -0800

    Task2: - [DELETE] /api/posts/:id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -72,6 +72,28 @@ router.post('/', (req, res) => {
     }
  });
  
- 
+router.delete('/:id', (req, res) => {
+    let deletedPost = {}
+    Post.findById(req.params.id)
+		.then(post => {
+			deletedPost = post							
+		})        
+
+    Post.remove(req.params.id)
+      .then(count => {
+        if (count > 0) {
+            res.status(200).json(deletedPost); 
+        } else {
+          res.status(404).json({ message: "The post with the specified ID does not exist" });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            message: "The post could not be removed"
+        })
+      })
+
+}); 
 
 module.exports = router
